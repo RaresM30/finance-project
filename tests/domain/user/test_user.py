0d0239968f5ed5fe228f49097f5ede063aa01fed
Rares <raresmarcus03@gmail.com>
2023-03-16 21:05:16 +0200

    Add the main user object

---
+++ tests/domain/user/test_user.py
@@ -0,0 +1,23 @@
+import unittest
+from domain.user.user import User
+
+
+class UserTestCase(unittest.TestCase):
+    def test_user_sets_the_right_username(self):
+        # setup
+        username = "random.generated"
+        user = User(username)
+        # execution
+        actual_username = user.username
+        # assertion
+        self.assertEqual(username, actual_username)
+
+    @unittest.skip("TODO: homework")
+    def test_it_sets_empty_list_if_we_do_not_specify_stocks(self):
+        pass
+
+    @unittest.skip("TODO: homework")
+    def test_it_sets_the_stocks_we_give(self):
+        pass
+
+        # set a list of 3 strings
