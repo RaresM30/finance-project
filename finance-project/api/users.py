f98944ccb150cd7b8136aac300f0996435cec76b
Rares <raresmarcus03@gmail.com>
2023-03-23 20:18:57 +0200

    Add api endpoints for read/create user

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,13 +1,30 @@
 from fastapi import APIRouter
+from domain.user.factory import UserFactory
+from domain.user.repo import UserRepo
+from api.models import UserAdd, UserInfo
+
 
 users_router = APIRouter(prefix="/users")
 
+repo = UserRepo("main_users.json")
 
-@users_router.get("/")
+
+@users_router.get("", response_model=list[UserInfo])
 def get_all_users():
-    return []
+    return repo.get_all()
 
 
-@users_router.post("/")
-def create_a_user():
+@users_router.get("/{username}", response_model=UserInfo)
+def get_user(username: str):
+    return repo.get_by_username(username)
+
+
+@users_router.post("")
+def create_a_user(new_user: UserAdd):
+    user = UserFactory().make(new_user.username)
+    repo.add(user)
+
+
+@users_router.delete("")
+def delete_a_user():
     pass
