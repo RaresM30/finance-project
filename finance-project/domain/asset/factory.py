c337c7600b8ba33a5d6fb74baebc0ef68dd116f8
Rares <raresmarcus03@gmail.com>
2023-03-28 20:57:48 +0300

    Add asset to domain

---
+++ finance-project/domain/asset/factory.py
@@ -0,0 +1,13 @@
+from domain.asset.asset import Asset
+import yahooquery
+
+
+class AssetFactory:
+    def make_new(self, ticker: str) -> Asset:
+        # TODO error handling&tests
+        t = yahooquery.Ticker(ticker)
+        profile = t.summary_profile[ticker]
+        name = profile["longBusinessSummary"].split(" ")[0:2]
+        country = profile["country"]
+        sector = profile["sector"]
+        return Asset(ticker=ticker, nr=0, name=name, country=country, sector=sector)
