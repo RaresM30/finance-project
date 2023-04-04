e9bb1be0f94aa858fdfab69af2c4800ca16e32af
Rares <raresmarcus03@gmail.com>
2023-04-04 20:39:25 +0300

    Extract file persistance logic in it s own class

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter
 from domain.asset.factory import AssetFactory
 from domain.user.factory import UserFactory
 from domain.user.repo import UserRepo
-from api.models import UserAdd, UserInfo, AssetInfo
+from api.models import UserAdd, UserInfo, AssetInfoUser
+from persistence.user_file import UserPersistenceFile
 
 users_router = APIRouter(prefix="/users")
 
-repo = UserRepo("main_users.json")
+user_persistence = UserPersistenceFile("main_users.json")
+repo = UserRepo(user_persistence)
 
 
 @users_router.get("", response_model=list[UserInfo])
@@ -16,12 +18,22 @@ def get_all_users():
 
 
 # todo get /users/{user_id}
+
+@users_router.get("/{user_id}", response_model=UserInfo)
+def get_user_by_id(user_id: str):
+    return repo.get_by_id(user_id)
+
+
 @users_router.get("/{username}", response_model=UserInfo)
 def get_user(username: str):
     return repo.get_by_username(username)
 
 
 # todo delete a user, DELETE /USERS/{user_id}
+
+@users_router.delete("/{user_id}")
+#def delete_a_user(user_id: str):
+    #return repo.delete_by_id(user_id)
 
 
 @users_router.post("", response_model=UserInfo)
@@ -32,7 +44,7 @@ def create_a_user(new_user: UserAdd):
 
 
 # TODO fix api, return asset info
-@users_router.post("/{user_id}/assets", response_model=AssetInfo)
+@users_router.post("/{user_id}/assets", response_model=AssetInfoUser)
 def add_asset_to_user(user_id: str, ticker: str):
     asset = AssetFactory().make_new(ticker)
     print(asset.__dict__)
--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -1,24 +1,28 @@
 import json
 
 from domain.user.factory import UserFactory
+from domain.user.persistence_interface import UserPersistenceInterface
 from domain.user.user import User
 
-
+#todo singleton, no hw
 class UserRepo:
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-        self.__load(file_path)
+    def __init__(self, persistence: UserPersistenceInterface):
+        self.__persistence = persistence
+        self.__users = None
+
 
     def add(self, new_user: User):
+        #todo refactor to not have duplicate code + add for get_by_id
+        if self.__users is None:
+            self.__users = self.__persistence.get_all()
         self.__users.append(new_user)
-        users_info = [(str(x.id), x.username, x.stocks) for x in self.__users]
-        users_json = json.dumps(users_info)
-        # todo refactor with
-        file = open(self.file_path, "w")
-        file.write(users_json)
-        file.close()
+        self.__persistence.add(new_user)
+
+
 
     def get_all(self) -> list[User]:
+        if self.__users is None:
+            self.__users = self.__persistence.get_all()
         return self.__users
 
     def get_by_username(self, username: str) -> User:
@@ -26,13 +30,12 @@ class UserRepo:
             if u.username == username:
                 return u
 
-    def __load(self, file_path):
-        try:
-            file = open(file_path)
-            contents = file.read()
-            file.close()
-            users_info = json.loads(contents)
-            factory = UserFactory()
-            self.__users = [factory.make_from_persistance(x) for x in users_info]
-        except:
-            self.__users = []
+    def get_by_id(self, id_: str) -> User:
+        for u in self.__users:
+            if str(u.id) == id_:
+                return u
+
+
+
+
+
