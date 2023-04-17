8db613199c6a3abfa826b4fd6f2009e9c7b498bd
Rares <raresmarcus03@gmail.com>
2023-04-17 13:33:33 +0300

    Add get_by_id into persistence

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -1,41 +1,34 @@
 import json
-
+import uuid
+from singleton import singleton
+from domain.asset.repo import AssetRepo
 from domain.user.factory import UserFactory
 from domain.user.persistence_interface import UserPersistenceInterface
 from domain.user.user import User
 
-#todo singleton, no hw
+
+@singleton
 class UserRepo:
     def __init__(self, persistence: UserPersistenceInterface):
+        print("Init user repo")
         self.__persistence = persistence
         self.__users = None
 
-
     def add(self, new_user: User):
-        #todo refactor to not have duplicate code + add for get_by_id
-        if self.__users is None:
-            self.__users = self.__persistence.get_all()
-        self.__users.append(new_user)
+        self.__check_we_have_users()
         self.__persistence.add(new_user)
-
-
+        self.__users.append(new_user)
 
     def get_all(self) -> list[User]:
-        if self.__users is None:
-            self.__users = self.__persistence.get_all()
+        self.__check_we_have_users()
         return self.__users
 
-    def get_by_username(self, username: str) -> User:
-        for u in self.__users:
-            if u.username == username:
-                return u
+    def get_by_id(self, uid: str) -> User:
+        self.__check_we_have_users()
+        return self.__persistence.get_by_id(uid)
 
-    def get_by_id(self, id_: str) -> User:
-        for u in self.__users:
-            if str(u.id) == id_:
-                return u
-
-
-
+    def __check_we_have_users(self):
+        if self.__users is None:
+            self.__users = self.__persistence.get_all()
 
 
