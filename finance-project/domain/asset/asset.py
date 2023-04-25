129c7df58975309a498c3d12809f839d6eafdcfa
Rares <raresmarcus03@gmail.com>
2023-04-25 17:59:18 +0300

    Add percentage

--- finance-project/domain/asset/asset.py
+++ finance-project/domain/asset/asset.py
@@ -1,4 +1,6 @@
 import yfinance
+
+
 class Asset:
     def __init__(self, ticker: str, nr: float, name: str, country: str, sector: str):
         self.__ticker = ticker
@@ -8,13 +10,11 @@ class Asset:
         self.__sector = sector
         yfin = yfinance.Ticker(ticker)
         self.__info = yfin.fast_info
-        #print(self.__yfin.fast_info)
-
+        # print(self.__yfin.fast_info)
 
     @property
     def ticker(self) -> str:
         return self.__ticker
-
 
     @property
     def units(self) -> float:
@@ -27,13 +27,16 @@ class Asset:
     @property
     def country(self) -> str:
         return self.__country
+
     @property
     def current_price(self) -> float:
         price = self.__info["lastPrice"]
         return round(price, 2)
+
     @property
     def currency(self) -> str:
         return self.__info["currency"]
+
     @property
     def closed_price(self) -> float:
         return self.__info["previousClose"]
@@ -42,7 +45,6 @@ class Asset:
     def fifty_day_price(self) -> float:
         return self.__info["fiftyDayAverage"]
 
-
     @property
     def today_low_price(self) -> float:
         return self.__info["dayLow"]
@@ -50,17 +52,23 @@ class Asset:
     @property
     def today_high_price(self) -> float:
         return self.__info["dayHigh"]
+
     @property
     def open_price(self) -> float:
         return self.__info["open"]
 
-
-
-    #TODO a property, in percentage how much it went up or down
-    # current_price & closed_price
-
-
-
-
-
-
+    @property
+    def percentage_diff(self) -> str:
+        diff = self.closed_price - self.current_price
+        percent_diff = (diff / self.closed_price) * 100
+        if diff > 0:
+            return (
+                f"The closed price {self.closed_price} is {percent_diff:.2f}%"
+                f" higher than the current price {self.current_price}"
+            )
+        elif diff < 0:
+            return (
+                f"The closed {self.closed_price} is {abs(percent_diff):.2f}%"
+                f" lower than current {self.current_price}"
+            )
+        return "The values are the same"
