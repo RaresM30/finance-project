64656d61f9c6e83851f3447a5a963f8c5c2a42ef
Rares <raresmarcus03@gmail.com>
2023-04-25 20:18:44 +0300

    Add description for each field

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -18,27 +18,27 @@ class AssetAdd(BaseModel):
 
 
 class AssetInfoBase(BaseModel):
-    ticker: str
-    name: str
-    country: str
-    sector: str
+    ticker: str = Field(description="A code that identifies the asset")
+    name: str = Field(description="The name that describes the chosen asset")
+    country: str = Field(description="The country where the asset first started")
+    sector: str = Field(description="The sector where the asset belongs")
 
 
 class AssetInfoUser(AssetInfoBase):
-    units: float
+    units: float = Field("Number of units of the asset")
 
 
 class AssetInfoPrice(AssetInfoBase):
-    current_price: float
-    currency: str
-    today_low_price: float
-    today_high_price: float
-    open_price: float
-    closed_price: float
-    fifty_day_price: float
+    current_price: float = Field(description="The current price of the asset")
+    currency: str = Field(description="The actual currency")
+    today_low_price: float = Field(description="Lowest price for the current day")
+    today_high_price: float = Field(description="Highest price for the current day")
+    open_price: float = Field(description="Opening price for the asset in the current day")
+    closed_price: float = Field(description="Closing price for the asset in the previous day")
+    fifty_day_price: float = Field(description="The average price over the past fifty days")
 
 
 class UserInfo(OrmModel):
     id: UUID = Field(description="An ID by which to identify a user")
     username: str
-    stocks: list[AssetInfoBase]
+    stocks: list[AssetInfoBase] = Field("A list of stocks")
