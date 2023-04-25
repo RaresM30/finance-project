f8e8ebfd186f0a49fc25e9638035fa4144b87170
Rares <raresmarcus03@gmail.com>
2023-04-25 18:00:01 +0300

    Add refactor for OrmModel

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -2,9 +2,16 @@ from pydantic import BaseModel, Field
 from uuid import UUID
 
 
-#todo add field with description, aprox half
+# todo add field with description, aprox half
+
+class OrmModel(BaseModel):
+    class Config:
+        orm_mode = True
+
+
 class UserAdd(BaseModel):
     username: str = Field(description="Alphanumeric username between 6 and 20 chars!")
+
 
 class AssetAdd(BaseModel):
     ticker: str
@@ -14,28 +21,24 @@ class AssetInfoBase(BaseModel):
     ticker: str
     name: str
     country: str
-    #TODO   refactor to not have duplicate code
+    sector: str
 
-    class Config:
-        orm_mode = True
 
 class AssetInfoUser(AssetInfoBase):
     units: float
 
-class AssetInfoPrice(AssetInfoBase):
-     current_price: float
-     currency: str
-     today_low_price: float
-     today_high_price: float
-     open_price: float
-     closed_price: float
-     fifty_day_price: float
 
-class UserInfo(BaseModel):
+class AssetInfoPrice(AssetInfoBase):
+    current_price: float
+    currency: str
+    today_low_price: float
+    today_high_price: float
+    open_price: float
+    closed_price: float
+    fifty_day_price: float
+
+
+class UserInfo(OrmModel):
     id: UUID = Field(description="An ID by which to identify a user")
     username: str
     stocks: list[AssetInfoBase]
-
-    class Config:
-        orm_mode = True
-
