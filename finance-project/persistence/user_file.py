8a23bcc75d993473957526df6926bb0c99dba3ae
Rares <raresmarcus03@gmail.com>
2023-04-25 18:02:20 +0300

    Add log&edit

--- finance-project/persistence/user_file.py
+++ finance-project/persistence/user_file.py
@@ -1,6 +1,6 @@
 import json
 import uuid
-
+import logging
 from domain.asset.repo import AssetRepo
 from domain.user.factory import UserFactory
 from domain.user.persistence_interface import UserPersistenceInterface
@@ -20,16 +20,15 @@ class UserPersistenceFile(UserPersistenceInterface):
             users_info = json.loads(contents)
             factory = UserFactory()
             return [factory.make_from_persistence(x) for x in users_info]
-        except:
-            #todo hw, log error
-                return []
+        except Exception as e:
+            logging.warning("We couldn't read the file, reason: " + str(e))
+            return []
 
     def add(self, user: User):
         current_users = self.get_all()
         current_users.append(user)
         users_info = [(str(x.id), x.username, x.stocks) for x in current_users]
         users_json = json.dumps(users_info)
-        # todo refactor with
         file = open(self.__file_path, "w")
         file.write(users_json)
         file.close()
@@ -46,5 +45,21 @@ class UserPersistenceFile(UserPersistenceInterface):
                 )
 
     def delete_by_id(self, uid: str):
-        pass
+        current_users = self.get_all()
+        users_without_id = [u for u in current_users if u.id != uuid.UUID(hex=uid)]
+        users_info = [(str(x.id), x.username, x.stocks) for x in users_without_id]
+        json_current_users = json.dumps(users_info)
+        with open(self.__file_path, "w") as f:
+            f.write(json_current_users)
+
+    def edit(self, user_id: str, username: str):
+        current_users = self.get_all()
+        for user in current_users:
+            if user.id == uuid.UUID(hex=user_id):
+                user.username = username
+        users_info = [(str(u.id), u.username, u.stocks) for u in current_users]
+        users_json = json.dumps(users_info)
+        with open(self.__file_path, "w") as f:
+            f.write(users_json)
+
 
