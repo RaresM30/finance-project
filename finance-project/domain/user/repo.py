299d0ba7d006b2dac1be157ef6b9b60d3e24bf9c
Rares <raresmarcus03@gmail.com>
2023-05-07 20:09:29 +0300

    Add asset in constructor for UserRepo

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -1,5 +1,8 @@
 import json
 import uuid
+
+from config.config_for_asset import check_asset_persistence_type
+from domain.asset.asset_persistence_interface import AssetPersistenceInterface
 from singleton import singleton
 from domain.asset.repo import AssetRepo
 from domain.user.factory import UserFactory
@@ -9,10 +12,11 @@ from domain.user.user import User
 
 @singleton
 class UserRepo:
-    def __init__(self, persistence: UserPersistenceInterface):
+    def __init__(self, persistence: UserPersistenceInterface, asset: AssetPersistenceInterface):
         print("Init user repo")
         self.__persistence = persistence
         self.__users = None
+        self.__asset = asset
 
     def add(self, new_user: User):
         self.__check_we_have_users()
@@ -25,7 +29,18 @@ class UserRepo:
 
     def get_by_id(self, uid: str) -> User:
         self.__check_we_have_users()
-        return self.__persistence.get_by_id(uid)
+
+        for u in self.__users:
+            if u.id == uuid.UUID(hex=uid):
+                asset_persistence = check_asset_persistence_type(
+                    "config.json"
+                )
+                assets = asset_persistence.get_all(u)
+                return User(
+                    uuid=u.id,
+                    username=u.username,
+                    stocks=assets,
+                )
 
     def delete_by_id(self, uid: str):
         self.__check_we_have_users()
