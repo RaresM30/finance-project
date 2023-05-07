b9b82b6683138542800c98c3aabef17933efc782
Rares <raresmarcus03@gmail.com>
2023-05-07 20:10:45 +0300

    Add test for stocks and id

--- tests/domain/user/test_user.py
+++ tests/domain/user/test_user.py
@@ -1,26 +1,48 @@
 import unittest
-from domain.user.new_user import User
+import uuid
+
+from domain.asset.asset import Asset
+from domain.user.user import User
 
 
-class UserTestCase(unittest.TestCase):
+class TestsForUser(unittest.TestCase):
     def test_user_sets_the_right_username(self):
         # setup
+        id_ = uuid.uuid4()
         username = "random.generated"
-        user = User(username)
+        user = User(id_, username)
         # execution
         actual_username = user.username
         # assertion
         self.assertEqual(username, actual_username)
 
     def test_it_sets_empty_list_if_we_do_not_specify_stocks(self):
-        user = User("random-username")
+        id_ = uuid.uuid4()
+        user = User(id_, "random-username")
 
         actual_stocks = user.stocks
 
         self.assertEqual([], actual_stocks)
 
-    @unittest.skip("TODO: homework")
     def test_it_sets_the_stocks_we_give(self):
-        pass
+        id_ = uuid.uuid4()
+        username = "random-name"
 
-        # set a list of 3 strings
+        actual_asset = [
+            Asset(country="United States", ticker="tsla", nr=0, name="Tesla", sector="Tech")
+        ]
+
+        user = User(id_, username, actual_asset)
+
+        actual = user.stocks
+
+        self.assertEqual(actual_asset, actual)
+
+    def test_it_sets_the_id(self):
+        id_ = uuid.uuid4()
+        user = User(id_, "user-test")
+        actual_id_for_user = user.id
+        self.assertEqual(actual_id_for_user, id_)
+
+if __name__ == "__main__":
+    unittest.main()
