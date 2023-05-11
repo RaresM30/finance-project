d114bbb395a177d7651a0972750246c6ea23b05b
Rares <raresmarcus03@gmail.com>
2023-05-11 20:21:12 +0300

    Added final updates

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -4,7 +4,6 @@ import yahooquery
 
 class AssetFactory:
     def make_new(self, ticker: str) -> Asset:
-        # TODO error handling&tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
         name = self.__extract_name(profile)
